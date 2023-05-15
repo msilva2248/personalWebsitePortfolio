56ff08a58d021954d0524f5fa45eb6bcc12da347
Matt <matthewsilva2248@gmail.com>
2023-05-15 13:02:09 -0600

    Commiting updated work to repo

--- Backend/Backend/src/main/java/com/MySQLConnection/Backend/controller/AboutMeController.java
+++ Backend/Backend/src/main/java/com/MySQLConnection/Backend/controller/AboutMeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin("http://personalwebportfolio.s3-website-us-west-1.amazonaws.com/")
 public class AboutMeController {
 
     @Autowired
--- Backend/Backend/src/main/java/com/MySQLConnection/Backend/controller/PortfolioController.java
+++ Backend/Backend/src/main/java/com/MySQLConnection/Backend/controller/PortfolioController.java
@@ -1,2 +1,29 @@
-package com.MySQLConnection.Backend.controller;public class PortfolioController {
+package com.MySQLConnection.Backend.controller;
+
+import com.MySQLConnection.Backend.model.Portfolio;
+import com.MySQLConnection.Backend.repository.PortfolioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+
+public class PortfolioController {
+
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    @PostMapping("/portfolio")
+    Portfolio newPortfolio(@RequestBody Portfolio newPortfolio) {
+        return portfolioRepository.save(newPortfolio);
+    }
+
+    @GetMapping("/portfolio/projects")
+    List<Portfolio> getAllPortfolioProjects() {
+        return portfolioRepository.findAll();
+    }
 }
